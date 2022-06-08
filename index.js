8299fb380fce1d5e9c105aa96a5539419010e058
sukanyagovindan <gsukanya@gmail.com>
2022-06-07 21:58:29 -0700

    Submit Assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber) {
   // Write your code here!
+  if (someNumber <= 400) {
+    return "This one is on me!";
+  } else if (someNumber > 400 & someNumber <= 2000) {
+    return "That will be twenty bucks."
+  } else if (someNumber > 2500) {
+    return "No can do."
+  } else if (someNumber > 2000 & someNumber <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(someString) {
 
-function switchOnCharmFromTip(){
+  return someString == 'NYC' ? "Ok, sounds good." : "No go.";
+
+}
+// Write your code here!
+
+
+function switchOnCharmFromTip(someString) {
   // Write your code here!
+  switch (someString) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
